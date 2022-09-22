b72e77c4f14122d39a81534ca686fd7ad00a76f1
tkyg <80114433+tkyg@users.noreply.github.com>
2022-09-22 14:30:36 -0400

    Done

---
+++ lib/user.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class User
+
+  def first_name= first_name
+    @first_name = first_name
+  end
+
+  def last_name= last_name
+    @last_name = last_name
+  end
+
+  def first_name
+    @first_name
+  end
+  def last_name
+    @last_name
+  end
+  # binding.pry
+end
